f9eee0e07882b5c095818f7c4b5e6929c7d30268
liangmuxue <liangmuxue@gmail.com>
2026-01-11 21:40:15 +0800

    sump att

--- custom/losses/mixer_loss.py
+++ custom/losses/mixer_loss.py
@@ -198,25 +198,18 @@ class FuturesIndustryLoss(UncertaintyLoss):
                         target_info_item = np.array(target_info[j])[ins_rel_index.cpu().numpy()]
                         # 使用价格指标作为主要指标
                         price_diff_range = price_targets[j,ins_rel_index]  
-                        round_targets_item_att = future_round_targets[j,ins_rel_index,self.cut_len-1,ref_indicator2]
+                        round_targets_item_att = future_round_targets[j,ins_rel_index,self.cut_len-1,ref_indicator]
                         cls_loss[i] += self.ccc_loss_comp(sv_out_item,round_targets_item)   
-                        ce_loss[i] += self.ccc_loss_comp(sv_out_item_att,round_targets_item_att)
                         # 计算top损失
-                        # ce_loss[i] += self.compute_top_loss(sv_out_item, round_targets_item, top_num=top_num)
+                        ce_loss[i] += self.compute_top_loss(sv_out_item, round_targets_item, top_num=top_num)
                         # target_item = target[j,main_index_abs,:,ref_indicator]
                         # price_last_target_items = target[j,ins_rel_index,-1,0]
                         # ce_loss[i] += self.compute_top_loss(sv_out_item_att2, price_last_target_items)
                         # target_total.append(target_item)                        
-                        # # 所有品种的目标阶段涨跌幅
-                        # price_diff_range_ins = np.array([self.compute_diff_range_class(item)[0] for item in target_info_item])
-                        # price_diff_range_ins = torch.Tensor(price_diff_range_ins).to(self.device) 
-                        # price_diff_range_ins = normalization_axis(price_diff_range_ins)
                         # 整体指数损失
-                        # fds_loss[i] += self.ccc_loss_comp(sw_index_data[j],target_item)
                         # fds_loss[i] += self.ccc_loss_comp(dec_out_item,target_item_ins)
                         # 辅助目标的损失
-                        
-                        # fds_loss[i] += self.ccc_loss_comp(sv_out_item_att2,round_targets_item_att)
+                        fds_loss[i] += self.ccc_loss_comp(sv_out_item_att,round_targets_item_att)
                         index_target_total.append(future_index_round_target[j,main_index,target_len,ref_indicator])
                         sw_index_total.append(sw_index_data[j])
                     elif target_mode==3:
