63b60621676ee2416b3cc4b74c0ea4db313b9e9d
liangmuxue <liangmuxue@gmail.com>
2026-07-06 22:43:39 +0800

    2022 ok

--- darts_pro/act_model/union_transformer.py
+++ darts_pro/act_model/union_transformer.py
@@ -579,7 +579,7 @@ class TFTWithFutureCovariatesDe(nn.Module):
         hist_summary = hist_summary * 0.8
         # 针对序列目标和单独阶段目标分别进行解码
         # pred_seq = self.seq_decoder(hist_summary,fut_proj)        # [B*S*P, obs_dim]
-        fur_scale = 0.01 # self.get_dynamic_target_ratio(current_epoch,max_epochs,final_target=1.0,warmup_ratio=0.1)
+        fur_scale = 0.001 # self.get_dynamic_target_ratio(current_epoch,max_epochs,final_target=1.0,warmup_ratio=0.1)
         self.fur_scale = fur_scale
         pred_tar = self.tar_decoder(hist_summary,future_single_emb,fur_scale=fur_scale)        # [B*S*1, obs_dim]
         # pred_tar_tmp = pred_tar.reshape([-1,pred_tar.shape[-1]])
